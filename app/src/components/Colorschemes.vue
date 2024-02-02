5dac7a811bc36cde9e185a7ea21ef5b4b31a6db9
kaizen-dw <dalewaltergh@gmail.com>
2024-02-02 18:47:31 +0800

    feat: Colorscheme buttons component

---
+++ app/src/components/Colorschemes.vue
@@ -0,0 +1,35 @@
+<script setup lang='ts'>
+  import { ref } from 'vue'
+
+  const colorGroupRef = ref<HTMLDivElement | null>(null)
+
+  const colorList = {
+    catppuccin: { 
+      primary: '#C6C6FA'
+    },
+    dracula: { 
+      primary: '#735AF6'
+    },
+    gruvbox: { 
+      primary: '#989520'
+    },
+  }
+
+  function handleClick(colorscheme: string) {
+    console.log('Color Button: ', colorscheme)
+  }
+
+</script>
+
+<template>
+  <div ref='colorGroupRef' class='absolute top-4 right-4 flex space-x-2'>
+    <button 
+      class='w-4 h-4 rounded-full'
+      v-for='(item, key) in colorList'
+      @click='handleClick(key)'
+      :style='{ background: item.primary }'
+    />
+  </div>
+</template>
+
+<!-- <style> </style> -->
